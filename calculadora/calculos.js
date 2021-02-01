cf614986b9348c0af521a8cd4633554bde1fda18
Yago Moreira <71896705+Yaguu@users.noreply.github.com>
2021-02-01 18:09:18 -0300

    Add files via upload

---
+++ calculadora/calculos.js
@@ -0,0 +1,8 @@
+function soma (a=0, b=0) {
+    console.log(a + b)
+    console.log(a - b)
+    console.log(a * b)
+    console.log(a / b)
+}
+
+soma ( , ) //digite o valor que você quer que seja calculado entre ()...
